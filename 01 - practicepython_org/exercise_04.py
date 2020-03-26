4186bec76ed793f90d43e10c3f19c303aadb2d7e
Rogerio Snarli <rogerio@snarli.no>
2020-03-27 00:13:40 +0100

    Divisors

---
+++ 01 - practicepython_org/exercise_04.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'pixelexio'
+__email__ = 'hello@pixelex.io'
+__license__ = 'GPLv3'
+
+""" 
+Exercise 04 - practicepython.org
+
+Create a program that asks the user for a number 
+and then prints out a list of all the divisors of that number.
+"""
+
+# get input with int validation
+print("")
+while True:
+  try:
+     num = int(input("Please enter a number to tp list the divisors: "))
+     break
+  except ValueError:
+     print("\nIvalid input. Input should be a integer.")
+     continue
+
+# loop range of numbers and make a list of divisors
+divisors = []
+for x in range(1,num+1):
+    if(num%x==0):
+        divisors.append(x)
+
+# print the result
+print("\nThe divisors of {} is:".format(num))
+print(*divisors, sep=", ")
