5b5a207b9bcf1b596fc738e6d9d824a6222a32ed
Rogerio Snarli <rogerio@snarli.no>
2020-03-26 23:33:24 +0100

    List less than x

---
+++ 01 - practicepython_org/exercise_03.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'pixelexio'
+__email__ = 'hello@pixelex.io'
+__license__ = 'GPLv3'
+
+""" 
+Exercise 03 - practicepython.org
+
+Write a program that prints out all the elements of the list that are less than 5.
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+Extras: 
+1. stead of printing the elements one by one, make a new list that has all 
+   the elements less than 5 from this list in it and print out this new list.
+2. Write this in one line of Python. 
+3. Ask the user for a number and return a list that contains only elements 
+   from the original list a that are smaller than that number given by the user
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print("\nOriginal list: \n{}".format(a))
+
+print("\nPrint elements lower than 5:")
+for x in a: 
+    if x < 5:
+        print(x)
+
+# Extras 1: make new lists with the elements
+print("\nExtras 1: Make a new list of the elements and print:")
+b = []
+for x in a: 
+    if x < 5:
+        b.append(x)
+print(b)
+
+print("\nPrint elements of the list without brackets:")
+print(*b, sep=", ")
+
+# Extras 2: One line code (two examples)
+print("\nExtras 2: Same code in one line (with and without brackets):")
+print([x for x in a if x < 5])
+print(*[x for x in a if x < 5], sep=", ")
+
+# Extras 3: User input
+print("\nWhat is the highest number you want to print?")
+
+while True:
+    num = input("Enter a number from 0 to 89: ")
+    if num.isnumeric() and int(num) in range(0, 90):
+        num = int(num)
+        break
+    print("\nIvalid input.")
+
+c = []
+for y in a: 
+    if y <= num:
+        c.append(y)
+if len(c) == 0:
+    print("\nThere is no element in the list equal or lower than {}:".format(num))
+elif len(a) == len(c):
+    print("\nAll elements in the list equal or lower than {}:".format(num))
+    print(c)
+if len(c) == 1: # in case the list is changes (lowest number of elements is 2 with current list).
+    print("\nThere is 1 element in the list equal or lower than {}:".format(num))
+else:
+    print("\nThere is {} elements in the list equal or lower than {}:".format(len(c), num))
+    print(c)
