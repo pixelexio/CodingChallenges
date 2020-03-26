c4b232518ae50aca4e4b814adeabdb538408ce6f
Rogerio Snarli <rogerio@snarli.no>
2020-03-26 21:42:53 +0100

    Odd and even numbers :)

--- 01 - practicepython_org/exercise_02.py
+++ 01 - practicepython_org/exercise_02.py
@@ -14,7 +14,7 @@ Depending on whether the number is even or odd, print out an appropriate message
 Extras: 
 1. If the number is a multiple of 4, print out a different message.
 2. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). 
-If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+If check divides evenly into num, tell that to the user. If not, print a different appropriate message. 
 """
 
 print("")
